650ef12ae4bfc99d45ac220aaccdc455a2cc35f7
prisharp <prisharp@umich.edu>
2025-03-25 12:32:12 -0400

    channelMLP

---
+++ channelMLP.py
@@ -0,0 +1,29 @@
+# FROM THE PAPER: 
+# “We apply a channel-mixing MLP, which contains two fully connected layers with a GELU activation layer between them. 
+# To make the MLP focus on local information, we add a depthwise convolution operation between the two FC layers.”
+
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ChannelMLP(nn.Module):
+    def __init__(self, channels):
+        super(ChannelMLP, self).__init__()
+        self.fc1 = nn.Linear(channels, channels * 2)
+        self.dwconv = nn.Conv2d(channels * 2, channels * 2, kernel_size=3, padding=1, groups=channels * 2)  # Depthwise conv
+        self.gelu = nn.GELU()
+        self.fc2 = nn.Linear(channels * 2, channels)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        x = x.permute(0, 2, 3, 1)  # B x H x W x C
+        x = self.fc1(x)
+        x = self.gelu(x)
+        x = x.permute(0, 3, 1, 2)  # B x C x H x W
+        x = self.dwconv(x)
+        x = x.permute(0, 2, 3, 1)  # B x H x W x C
+        x = self.fc2(x)
+        x = x.permute(0, 3, 1, 2)  # B x C x H x W
+        return x
