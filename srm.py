e80e87d12b5585d85d3d7fc44b0d4ccddf7d5499
761mengjiaz <mengjiaz@umich.edu>
2025-03-31 12:52:11 -0400

    Update srm.py
    just clarifying nothing in the spatial rearrangment unit has any changes it should be exactly the one kevin uploaded with some comments

--- srm.py
+++ srm.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Unchanged Spatial Rearrangment Unit from Kevin
 class SpatialRearrangementUnit(nn.Module):
     def __init__(self, window_size):
         """
